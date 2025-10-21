33c8be31cbf39689ee6ab0869b3ca482bd7e87a7
LostPatrol <2823931730@qq.com>
2025-10-22 00:05:13 +0800

    Implementation: Phantoms rocket charge ai and attacks

--- src/main/java/net/lostpatrol/mobspvpmaster/client/render/entity/EnhancedPhantomRenderer.java
+++ src/main/java/net/lostpatrol/mobspvpmaster/client/render/entity/EnhancedPhantomRenderer.java
@@ -6,6 +6,7 @@ import net.lostpatrol.mobspvpmaster.MobsPVPMaster;
 import net.lostpatrol.mobspvpmaster.client.render.entity.layers.PhantomRocketLayer;
 import net.lostpatrol.mobspvpmaster.client.render.entity.layers.PhantomWeaponLayer;
 import net.lostpatrol.mobspvpmaster.client.model.EnhancedPhantomModel;
+import net.lostpatrol.mobspvpmaster.util.Constants;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
@@ -16,6 +17,7 @@ import net.minecraft.world.entity.monster.Phantom;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 
 @OnlyIn(Dist.CLIENT)
@@ -32,20 +34,22 @@ public class EnhancedPhantomRenderer extends MobRenderer<Phantom, PhantomHolding
         logger.info("Added enhanced phantom renderer");
     }
 
-    public ResourceLocation getTextureLocation(PhantomHoldingRenderState phantomHoldingRenderState) {
+    @NotNull
+    public ResourceLocation getTextureLocation(@NotNull PhantomHoldingRenderState phantomHoldingRenderState) {
         return PHANTOM_LOCATION;
     }
 
+    @NotNull
     public PhantomHoldingRenderState createRenderState() {
         return new PhantomHoldingRenderState();
     }
 
-    public void extractRenderState(Phantom phantom, PhantomHoldingRenderState renderState, float partialTick) {
-//        if (! phantom.getPersistentData().getBooleanOr(ENHANCED_PHANTOM_BOOLEAN, false))
-//            return;
+    public void extractRenderState(@NotNull Phantom phantom, @NotNull PhantomHoldingRenderState renderState, float partialTick) {
         super.extractRenderState(phantom, renderState, partialTick);
+
         this.itemModelResolver.updateForLiving(renderState.mainHandItem, phantom.getMainHandItem(), ItemDisplayContext.GROUND, phantom);
         this.itemModelResolver.updateForLiving(renderState.offhandItem, phantom.getOffhandItem(), ItemDisplayContext.GROUND, phantom);
+
         renderState.flapTime = phantom.getUniqueFlapTickOffset() + renderState.ageInTicks;
         renderState.size = phantom.getPhantomSize();
     }
@@ -56,7 +60,7 @@ public class EnhancedPhantomRenderer extends MobRenderer<Phantom, PhantomHolding
         poseStack.translate(0.0F, 1.3125F, 0.1875F);
     }
 
-    protected void setupRotations(PhantomHoldingRenderState renderState, PoseStack poseStack, float bodyRot, float scale) {
+    protected void setupRotations(@NotNull PhantomHoldingRenderState renderState, @NotNull PoseStack poseStack, float bodyRot, float scale) {
         super.setupRotations(renderState, poseStack, bodyRot, scale);
         poseStack.mulPose(Axis.XP.rotationDegrees(renderState.xRot));
     }
--- src/main/java/net/lostpatrol/mobspvpmaster/event/equips/PhantomEquipmentHandler.java
+++ src/main/java/net/lostpatrol/mobspvpmaster/event/equips/PhantomEquipmentHandler.java
@@ -1,7 +1,10 @@
 package net.lostpatrol.mobspvpmaster.event.equips;
 
 import net.lostpatrol.mobspvpmaster.MobsPVPMaster;
+import net.lostpatrol.mobspvpmaster.entity.ai.PhantomRocketChargeAttackGoal;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.goal.WrappedGoal;
 import net.minecraft.world.entity.monster.Phantom;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -9,27 +12,48 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 
-import static net.lostpatrol.mobspvpmaster.util.Constants.ENHANCED_PHANTOM_BOOLEAN;
+import net.lostpatrol.mobspvpmaster.util.Constants;
+import org.slf4j.Logger;
 
 @EventBusSubscriber(modid = MobsPVPMaster.MODID)
 public class PhantomEquipmentHandler {
+    public static Logger logger = MobsPVPMaster.LOGGER;
+
     @SubscribeEvent
     public static void onPhantomSpawn(EntityJoinLevelEvent event) {
         if (event.getLevel().isClientSide()) return;
 
         if (event.getEntity() instanceof Phantom phantom) {
             if (phantom.getRandom().nextFloat() < 0.9f) {
-
-                phantom.getPersistentData().putBoolean(ENHANCED_PHANTOM_BOOLEAN, true);
+                phantom.getPersistentData().putBoolean(Constants.ENHANCED_PHANTOM_BOOLEAN, true);
 
                 ItemStack diamondSword = new ItemStack(Items.DIAMOND_SWORD);
                 phantom.setItemSlot(EquipmentSlot.MAINHAND, diamondSword);
                 ItemStack rocket = new ItemStack(Items.FIREWORK_ROCKET, 3 + event.getLevel().getDifficulty().ordinal());
                 phantom.setItemSlot(EquipmentSlot.OFFHAND, rocket);
 
-
-//                phantom.goalSelector.addGoal(1, new PhantomRocketChargeAttackGoal());
+                rerankPhantomGoals(phantom);
             }
         }
+    }
+
+    public static void rerankPhantomGoals(Phantom phantom) {
+        Goal vanillaAttackGoal =  null;
+        Goal vanillaCircleGoal = null;
+        for (WrappedGoal wrappedGoal : phantom.goalSelector.getAvailableGoals()) {
+            Goal goal = wrappedGoal.getGoal();
+            if (goal instanceof Phantom.PhantomSweepAttackGoal) {
+                vanillaAttackGoal = goal;
+                phantom.goalSelector.removeGoal(goal);
+            } else if (goal instanceof Phantom.PhantomCircleAroundAnchorGoal) {
+                vanillaCircleGoal = goal;
+                phantom.goalSelector.removeGoal(goal);
+            }
+        }
+
+        // priority 1: strategyGoal
+        phantom.goalSelector.addGoal(2, new PhantomRocketChargeAttackGoal(phantom));
+        phantom.goalSelector.addGoal(3, vanillaAttackGoal);
+        phantom.goalSelector.addGoal(4, vanillaCircleGoal);
     }
 }
